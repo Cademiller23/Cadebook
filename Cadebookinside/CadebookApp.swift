de249f23b2df613cbe846a043bb4ff57febd2a68
Cade Miller <Caderyland1@gmail.com>
2022-01-09 19:09:26 -0800

    no message

---
+++ Cadebookinside/CadebookApp.swift
@@ -0,0 +1,18 @@
+//
+//  CadebookApp.swift
+//  Cadebook
+//
+//  Created by Cade Miller on 1/6/22.
+//
+
+import SwiftUI
+
+@main
+struct CadebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+         
+        }
+    }
+}
